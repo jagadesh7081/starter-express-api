adf3de9c1e6ced2e688982d9e1d0be84f89d75bd
Jagadesh S <43233570+jagadesh7081@users.noreply.github.com>
2022-12-27 14:48:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('Yo! what the hell is this cyclic')
 })
 app.listen(process.env.PORT || 3000)
